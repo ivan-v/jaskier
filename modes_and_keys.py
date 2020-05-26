2326040dc9998e990f435f5e61b40925a6dc6ed4
Ivan Viro <navioriv@gmail.com>
2020-05-25 22:42:19 -0400

    Added new scales from David Cope's book

--- modes_and_keys.py
+++ modes_and_keys.py
@@ -6,9 +6,23 @@ Modes = {
   "Dorian":     [0, 2, 3, 5, 7, 9, 10],
   "Phrygian":   [0, 1, 3, 5, 7, 8, 10],
   "Aeolian":    [0, 2, 3, 5, 7, 8, 10],
+  
+
+  # from David Cope's "Techniques of the Contemporary Composer"
+  "Alaska":           [0, 3, 5, 7, 8, 10, 11],
+  "Jewish":           [0, 1, 4, 5, 7, 8, 10],
+  "India":            [0, 2, 3, 6, 7, 9, 11],
+  "Spain":            [0, 1, 3, 5, 7, 8, 11],
+  "Asian-Bartok":     [0, 2, 3, 6, 7, 9, 10],
+  "Hungarian-Bartok": [0, 2, 4, 6, 7, 9, 10],
+  "Bulgarian-Bartok": [0, 1, 3, 5, 7, 9, 10],
+
   # TODO: make non-8 note scales work properly
   "Pentatonic": [0, 2, 4, 6, 9],
   "Blues":      [0, 3, 5, 6, 7, 10],
+  "Japan":      [0, 2, 4, 5, 6, 7, 9, 10, 11],
+  "Peru":       [0, 3, 5, 7, 10],
+  "Slavic":     [0, 5, 7, 8],
 
 }
 
