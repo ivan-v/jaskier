1674bcbe882824b29da27d1cc042e0d79cd2a725
ivan-v <navioriv@gmail.com>
2020-07-04 12:58:57 -0400

    Add main.py (the flask app itself)

---
+++ main.py
@@ -0,0 +1,38 @@
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+# [START gae_python37_app]
+from flask import Flask, send_from_directory
+from jazz_improvisation import test
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+app.config["CLIENT_DOWNLOADS"] = "/Users/ivan/Downloads/python-docs-samples/appengine/standard_python37/hello_world/"
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    test()
+    return send_from_directory(app.config["CLIENT_DOWNLOADS"], filename="jazz_improv.mid")
+    # return 'Hello World! -Ivan'
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run(host='127.0.0.1', port=8013, debug=True)
+# [END gae_python37_app]
